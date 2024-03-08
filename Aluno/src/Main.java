eda3bc6ac6a5a258fe655ff57724d76b51cc3196
Renan Germano <61218420+renangfs@users.noreply.github.com>
2024-03-08 09:37:33 -0300

    Update Main.java

--- Aluno/src/Main.java
+++ Aluno/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno();
-        //add segundo push
+        
         aluno1.setNome("Renan");
         aluno1.setMatricula("12345");
         aluno1.setIdade(20);
