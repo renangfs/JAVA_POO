94fff19b413f3f4696c724e1f8189bbaa7ab43a9
Renan Germano <61218420+renangfs@users.noreply.github.com>
2024-03-08 10:19:20 -0300

    Add files via upload

---
+++ ConversorTemperatura/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double celsius;
+        double fahrenheit;
+
+        System.out.println("Digite a temperatura em Celsius para a conversão:...");
+        celsius = scanner.nextDouble();
+
+        fahrenheit = (celsius * 1.8) + 32;
+        System.out.printf("A temperatura de %.1f graus celsius corresponde a %.1f fahrenheit%n",celsius,fahrenheit);
+
+
+    }
+}
